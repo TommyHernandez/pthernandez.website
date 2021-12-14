a1ea5247cb423ce63354b6934c702e28ddba5f01
Pedro Hernández <pedrothdc@gmail.com>
2021-12-14 20:33:59 +0100

    Added new icon Completed basic post style

--- pages/index/posts/_slug.vue
+++ pages/index/posts/_slug.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="article">
     <div class="article__topbar">
-      <nuxt-link to="/blog"> Volver </nuxt-link>
+      <nuxt-link to="/blog">
+        <Icon icon="fluent:arrow-step-back-16-regular" /> Volver
+      </nuxt-link>
     </div>
     <article>
+      <img :src="'/' + post.image" alt="Imagen principal del post" />
       <h1>
         {{ post.title }}
       </h1>
@@ -12,7 +15,12 @@
   </section>
 </template>
 <script>
+import { Icon } from '@iconify/vue2';
+
 export default {
+  components: {
+    Icon,
+  },
   async asyncData({ $content, params, error }) {
     const post = await $content(`posts/${params.slug}`)
       .fetch()
@@ -26,14 +34,33 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+@use '../../../assets/tools/mixins';
+
 .article {
-  &__content {
-    max-width: 850px;
-    margin: 0 auto;
-    p {
-      margin-bottom: 12px;
+  max-width: 850px;
+  margin: 0 auto;
+  &__topbar {
+    padding: 12px 0 32px 0;
+    a {
+      display: block;
+      @include mixins.fontBlack;
+      text-decoration: none;
+      line-height: 1.1rem;
     }
+  }
+  &__content {
+    p {
+      margin: 0 auto;
+      margin-bottom: 32px;
+      font-size: 18px;
+      line-height: 28px;
+      word-break: break-word;
+    }
+  }
+  h1 {
+    font-size: 2.1rem;
+    margin-bottom: 16px;
   }
 }
 </style>
